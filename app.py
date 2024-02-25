b343b2714130e2371f610d78ad4790380af7e643
OveRide <shashanksrao@gmail.com>
2024-02-25 22:54:46 +0530

    Update app.py
    added info button

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
-from file_search import search_files_by_tag
+from file_search import search_files_by_tag, query_metadata_from_database
 from subprocess import run
 from datetime import datetime
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ def home():
     if request.method == 'POST':
         google_drive_link = request.form['google_drive_link']
         # Run GDrive_PreProcessing.py with the provided Google Drive link
-        run(['python', 'D:/PROJECTS/NEW_MTAM/GDrive_PreProcessing.py', google_drive_link])
+        run(['python', 'D:/PROJECTS_FINAL/DriveTrack/GDrive_PreProcessing.py', google_drive_link])
         # Redirect to index.html or any other page
         return render_template('index.html')
     return render_template('home.html')
@@ -34,6 +35,17 @@ def search_files():
         search_results = search_files_by_tag(search_term, date=date if date_str else None)
         return render_template('index.html', search_results=search_results, search_term=search_term)
     return render_template('search.html')
+
+@app.route('/get_file_metadata')
+def get_file_metadata():
+    filename = request.args.get('filename')
+
+    # Query the database for metadata based on the filename
+    # Replace this with your actual query
+    metadata = query_metadata_from_database(filename)
+
+    # Return metadata as JSON response
+    return jsonify(metadata)
 
 if __name__ == '__main__':
     app.run(debug=True)
